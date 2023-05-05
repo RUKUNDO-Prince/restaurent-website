17b077129ce0d8f7e68787ab4fdd201cb2bcf2eb
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-05-05 08:29:08 +0300

    react restaurent website

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { SubHeading, MenuItem } from '../../components';
-// import { images, data } from '../../constants/images';
+import { data, images } from '../../constants';
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
@@ -12,8 +12,26 @@ const SpecialMenu = () => (
     <div className="app__specialMenu-menu">
         <div className="app__specialMenu-menu_wine flex__center">
           <p className='app__specialMenu-menu_heading'>Wine & Beer</p>
-          <div className="app__specialMenu-menu_items"></div>
+          <div className="app__specialMenu-menu_items">
+            {data.wines.map((wine, index) => (
+              <p>{wine.title}</p>
+            ))}
+          </div>
         </div>
+        <div className="app__specialMenu-menu_img">
+          <img src={images.menu} alt="menu" />
+        </div>
+        <div className="app__specialMenu-menu_cocktails flex__center">
+          <p className='app__specialMenu-menu_heading'>Cocktails</p>
+          <div className="app__specialMenu-menu_items">
+            {data.cocktails.map((cocktail, index) => (
+              <p>{cocktail.title}</p>
+            ))}
+          </div>
+        </div>
+    </div>
+    <div style={{marginTop: '15px'}}>
+      <button type='button' className='custom__button'>View More</button>
     </div>
   </div>
 );
