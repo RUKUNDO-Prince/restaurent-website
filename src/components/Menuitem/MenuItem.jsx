c2151784da2b784e02d3680558b4062cc70eef52
RUKUNDO Princde <rukundoprince951@gmail.com>
2023-05-05 12:21:52 +0300

    react restaurent website

--- src/components/Menuitem/MenuItem.jsx
+++ src/components/Menuitem/MenuItem.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 import './MenuItem.css';
 
-const MenuItem = () => (
-  <div>
-    MenuItem
+const MenuItem = ({ title, price, tags }) => (
+  <div className='app__menuitem'>
+    <div className="app__menuitem-head">
+      <div className="app__menuitem-name">
+        <p className="p__cormorant" style={{color: '#dcca87'}}>{title}</p>
+      </div>
+      <div className="app__menuitem-dash" />
+      <div className="app__menuitem-price">
+        <p className="p__cormorant" style={{color: '#dcca87'}}>{price}</p>
+      </div>
+    </div>
   </div>
 );
 
